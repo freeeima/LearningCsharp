7cc35833fb89a841c5757ed93793c17f4cbf58e8
DESKTOP-3B0DNC5\Vartotojas <e.uselyte@gmail.com>
2021-04-07 16:55:59 +0300

    Project added

---
+++ LearningApp/Homework3/Homework3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningApp.Homework3
+{
+	class Homework3
+	{
+		static void Main(string[] args)
+		{
+			/* Pirma užduotis
+			Console.WriteLine("Įveskite tris skaičius:");
+			int Pirmas = Convert.ToInt32(Console.ReadLine());
+			int Antras = Convert.ToInt32(Console.ReadLine());
+			int Trečias = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Pirmas * Antras * Trečias);
+			Console.ReadLine(); 
+
+			*/
+
+
+			/* Antra užduotis
+			Console.WriteLine("Įveskite pirmą skaičių:");
+			int Pirmas = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Įveskite antrą skaičių:");
+			int Antras = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Sudėtis lygi " + (Pirmas + Antras));
+			Console.WriteLine("Atimtis lygi " + (Pirmas - Antras));
+			Console.WriteLine("Sandauga lygi " + (Pirmas * Antras));
+			double division = (double)Pirmas / Antras;
+			Console.WriteLine($"Dalyba lygi {division}");
+			Console.ReadLine();
+
+			*/
+
+
+			/* Trečia užduotis
+
+			Console.WriteLine("Įveskite pirmą skaičių:");
+			int Pirmas = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Įveskite antrą skaičių:");
+			int Antras = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Įveskite trečią skaičių:");
+			int Trecias = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Įveskite ketvirtą skaičių:");
+			int Ketvirtas = Convert.ToInt32(Console.ReadLine());
+			double average = (double)(Pirmas + Antras + Trecias + Ketvirtas) / 4;
+			Console.WriteLine($"Keturių skaičių vidurkis {Pirmas}, {Antras}, {Trecias} ir {Ketvirtas} yra " + average + ".");
+			Console.ReadLine();
+
+			*/
+
+
+		}
+	}
+}
---
+++ LearningApp/Lesson3/Program3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LearningApp
+{
+	class Program3
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Įveskite tris skaičius!");
+			int Pirmas = Convert.ToInt32(Console.ReadLine());
+			int Antras = Convert.ToInt32(Console.ReadLine());
+			int Trečias = Convert.ToInt32(Console.ReadLine());
+
+
+
+			switch (Pirmas)
+			{
+				case 1:
+					Console.WriteLine(Pirmas + Antras + Trečias);
+					break;
+				case 2:
+					Console.WriteLine(Pirmas - Trečias);
+					break;
+				case 3:
+					Console.WriteLine(Antras * Trečias);
+					break;
+				default:
+					Console.WriteLine("Klaida");
+					break;
+
+
+
+			}
+		}
+	}
+}
