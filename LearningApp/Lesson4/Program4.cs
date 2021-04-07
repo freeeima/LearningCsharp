66f80c0805763e5a89187120883f7ddf949e667e
DESKTOP-3B0DNC5\Vartotojas <e.uselyte@gmail.com>
2021-04-07 17:09:16 +0300

    Lesson4 created

---
+++ LearningApp/Lesson4/Program4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningApp.Lesson4
+{
+    class Program4
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi!");
+        }
+     }
+}
